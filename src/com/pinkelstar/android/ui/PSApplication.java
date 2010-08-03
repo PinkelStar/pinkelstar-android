02b3767a1c653ff6caacb7d14a1fe5e6090b244d
Arno den Hond <arnodenhond@gmail.com>
2010-08-03 14:13:50 +0200

    read settings from xml file in stead of strings

--- src/com/pinkelstar/android/ui/PSApplication.java
+++ src/com/pinkelstar/android/ui/PSApplication.java
@@ -1,7 +1,12 @@
 package com.pinkelstar.android.ui;
 
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParserException;
+
 import pinkelstar.android.R;
 import android.app.Application;
+import android.content.res.XmlResourceParser;
 
 import com.pinkelstar.android.server.Server;
 import com.pinkelstar.android.ui.tasks.SessionTask;
@@ -10,11 +15,23 @@ public class PSApplication extends Application implements PSApplicationState {
 
 	private Server psServer;
 	private ImageCache imageCache;
-	
+	String key;
+	String secret;
+	boolean preload;
+
 	public void onCreate() {
-		psServer = new Server((Application) this, getString(R.string.app_key), getString(R.string.app_secret));
-		imageCache  = new ImageCache();
 		
+		try {
+			loadSettings();
+		} catch (XmlPullParserException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		psServer = new Server((Application) this, key, secret);
+		imageCache = new ImageCache();
+
 		SessionTask.initialize(psServer, imageCache, getString(R.string.preloadimages));
 	}
 
@@ -25,5 +42,22 @@ public class PSApplication extends Application implements PSApplicationState {
 	public ImageCache getPinkelstarImageCache() {
 		return this.imageCache;
 	}
-	
+
+	private void loadSettings() throws XmlPullParserException, IOException {
+		XmlResourceParser parser = getResources().getXml(R.xml.settings);
+		while (parser.getName() == null || !parser.getName().equals("key"))
+			parser.next();
+		parser.next();
+		key = parser.getText();
+		while (parser.getName() == null || !parser.getName().equals("secret"))
+			parser.next();
+		parser.next();
+		secret = parser.getText();
+		
+		while (parser.getName() == null || !parser.getName().equals("preload"))
+				parser.next();
+		parser.next();
+		preload = parser.getText().equals("true");
+	}
+
 }
